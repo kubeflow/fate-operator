600cb4bd67c6fbb306763db3b49455e7b36815d4
Ce Gao <gaoce@caicloud.io>
2020-07-02 10:26:13 +0800

    fix: Fix import order
    Signed-off-by: Ce Gao <gaoce@caicloud.io>

--- controllers/fatejob_controller.go
+++ controllers/fatejob_controller.go
@@ -19,14 +19,13 @@ import (
 	"reflect"
 	"time"
 
-	"k8s.io/apimachinery/pkg/types"
-
 	"github.com/go-logr/logr"
 	batchv1 "k8s.io/api/batch/v1"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
